db2441323ccd504bb47d0a9575dffe9a3e0b007d
Damongsanga <mike0413lata@gmail.com>
2023-11-14 17:53:27 +0900

    docs: DB 설정정보

---
+++ docs/develop/DB/Schema.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `users` (
+  `user_id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `tier` integer NOT NULL,
+  `rank` integer NOT NULL,
+  `img_url` varchar(255) NOT NULL
+);
+
+CREATE TABLE `tier_problem` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `tier` integer NOT NULL,
+  `count` integer NOT NULL
+);
+
+CREATE TABLE `user_group` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `group_id` integer NOT NULL
+);
+
+CREATE TABLE `problem` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `problem_num` integer NOT NULL,
+  `tier` integer NOT NULL,
+  `title` varchar(255) NOT NULL
+);
+
+CREATE TABLE `problem_algorithm` (
+  `problem_num` integer PRIMARY KEY,
+  `algorithm` varchar(255) NOT NULL
+);
+
+CREATE TABLE `group` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `group_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `recom_problem` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `group_id` integer NOT NULL,
+  `problem_num` integer NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `tier` integer NOT NULL
+);
+
+CREATE TABLE `user_tier_problem` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `user_id` integer NOT NULL,
+  `tier` integer NOT NULL,
+  `problem_num` integer NOT NULL,
+  `title` varchar(255) NOT NULL
+);
+
+ALTER TABLE `tier_problem` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `user_group` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `problem` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `problem` ADD FOREIGN KEY (`problem_num`) REFERENCES `problem_algorithm` (`problem_num`);
+
+ALTER TABLE `user_group` ADD FOREIGN KEY (`group_id`) REFERENCES `group` (`id`);
+
+ALTER TABLE `recom_problem` ADD FOREIGN KEY (`group_id`) REFERENCES `group` (`id`);
+
+ALTER TABLE `recom_problem` ADD FOREIGN KEY (`problem_num`) REFERENCES `problem_algorithm` (`problem_num`);
+
+ALTER TABLE `user_tier_problem` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `user_tier_problem` ADD FOREIGN KEY (`problem_num`) REFERENCES `problem_algorithm` (`problem_num`);
