8c2bf9bb377a0acb85d0d45164d34b31927bde51
damongsanga <mike0413lata@gmail.com>
2023-11-19 23:32:06 +0900

    feat: RestTemplate 추가

--- springboot back/bboard/src/main/java/com/ssafypjt/bboard/util/UtilConfig.java
+++ springboot back/bboard/src/main/java/com/ssafypjt/bboard/util/UtilConfig.java
@@ -2,8 +2,10 @@ package com.ssafypjt.bboard.util;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
@@ -21,6 +23,9 @@ public class UtilConfig {
         return new ObjectMapper();
     }
 
-
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
 }
