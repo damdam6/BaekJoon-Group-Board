bf0d49ee25c8c57808e839ebf0ccccba7cffba6e
damdam6 <dambi626626@gmail.com>
2023-11-20 15:58:52 +0900

    feat: Vue Front생성

---
+++ vue front/src/components/CompareView.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="p-4 bg-white rounded-lg shadow sm:p-6 xl:p-8 ">
+        <h3 class="mb-10 text-xl font-bold leading-none text-gray-900">Acquisition Overview</h3>
+
+        <div class="block w-full overflow-x-auto">
+            <table class="items-center w-full bg-transparent border-collapse">
+                <thead>
+                    <tr>
+                        <th
+                            class="px-4 py-3 text-xs font-semibold text-left text-gray-700 uppercase align-middle border-l-0 border-r-0 bg-gray-50 whitespace-nowrap">
+                            Top Channels</th>
+                        <th
+                            class="px-4 py-3 text-xs font-semibold text-left text-gray-700 uppercase align-middle border-l-0 border-r-0 bg-gray-50 whitespace-nowrap">
+                            Users</th>
+                        <th
+                            class="px-4 py-3 text-xs font-semibold text-left text-gray-700 uppercase align-middle border-l-0 border-r-0 bg-gray-50 whitespace-nowrap min-w-140-px">
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-100">
+                    <tr class="text-gray-500">
+                        <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Organic Search</th>
+                        <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            5,649</td>
+                        <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">30%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 rounded-sm bg-cyan-600" style="width: 30%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="text-gray-500">
+                        <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Referral</th>
+                        <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            4,025</td>
+                        <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">24%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 bg-orange-300 rounded-sm" style="width: 24%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="text-gray-500">
+                        <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Direct</th>
+                        <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            3,105</td>
+                        <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">18%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 bg-teal-400 rounded-sm" style="width: 18%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="text-gray-500">
+                        <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Social</th>
+                        <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            1251</td>
+                        <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">12%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 bg-pink-600 rounded-sm" style="width: 12%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="text-gray-500">
+                        <th class="p-4 px-4 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Other</th>
+                        <td class="p-4 px-4 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            734</td>
+                        <td class="p-4 px-4 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">9%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 bg-indigo-600 rounded-sm" style="width: 9%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="text-gray-500">
+                        <th class="p-4 pb-0 text-sm font-normal text-left align-middle border-t-0 whitespace-nowrap">
+                            Email</th>
+                        <td class="p-4 pb-0 text-xs font-medium text-gray-900 align-middle border-t-0 whitespace-nowrap">
+                            456</td>
+                        <td class="p-4 pb-0 text-xs align-middle border-t-0 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <span class="mr-2 text-xs font-medium">7%</span>
+                                <div class="relative w-full">
+                                    <div class="w-full h-2 bg-gray-200 rounded-sm">
+                                        <div class="h-2 bg-purple-500 rounded-sm" style="width: 7%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+    },
+}
+</script>
