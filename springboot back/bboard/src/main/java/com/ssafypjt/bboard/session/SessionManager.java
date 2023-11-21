09e6b747b7fa8fc698578a7ea1516f352eed2fde
damongsanga <mike0413lata@gmail.com>
2023-11-21 23:08:18 +0900

    comment: 주석 변경

--- springboot back/bboard/src/main/java/com/ssafypjt/bboard/session/SessionManager.java
+++ springboot back/bboard/src/main/java/com/ssafypjt/bboard/session/SessionManager.java
@@ -9,9 +9,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * 세션 관리
- */
 @Component
 public class SessionManager {
 
@@ -22,9 +19,6 @@ public class SessionManager {
     // 동시성을 위해 ConcurrentHashMap<>() 사용
     private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
 
-    /**
-     * 세션 생성
-     */
     public void createSession(Object value, HttpServletResponse response) {
 
         // 세션 id를 생성하고, 값을 세션에 저장
@@ -39,23 +33,16 @@ public class SessionManager {
         response.addCookie(mySessionCookie);
     }
 
-    /**
-     * 세션 조회
-     */
     public Object getSession(HttpServletRequest request) {
         // 쿠기를 찾음
         Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
         if (sessionCookie == null) {
-            System.out.println("쿠키가 없어");
             return null;
         }
         System.out.println(sessionCookie.getValue());
         return sessionStore.get(sessionCookie.getValue());
     }
 
-    /**
-     * 세션 만료
-     */
     public void expire(HttpServletRequest request) {
         Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
         if (sessionCookie != null) {
@@ -64,7 +51,6 @@ public class SessionManager {
         }
     }
 
-    // 쿠키를 찾는 로직
     public Cookie findCookie(HttpServletRequest request, String cookieName) {
         if (request.getCookies() == null) {
             return null;
