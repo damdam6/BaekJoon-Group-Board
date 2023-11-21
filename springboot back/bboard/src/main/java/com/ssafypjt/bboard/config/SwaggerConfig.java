61616f840647d40924d3ee0a6d950a1b6018bddc
damongsanga <mike0413lata@gmail.com>
2023-11-21 20:43:46 +0900

    style: 주석수정

--- springboot back/bboard/src/main/java/com/ssafypjt/bboard/config/SwaggerConfig.java
+++ springboot back/bboard/src/main/java/com/ssafypjt/bboard/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 
 
 @Configuration
