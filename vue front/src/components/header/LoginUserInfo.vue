f2d85e96028328b1d5b54d30cd9743ade9bf43dc
damdam6 <dambi626626@gmail.com>
2023-11-21 11:54:05 +0900

    feat: Header 생성 및 수정
    close : #19

---
+++ vue front/src/components/header/LoginUserInfo.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex items-center">
+        <div class="items-center hidden lg:flex">
+            <span class="mr-5 text-base font-normal text-gray-500"> user_name ❤️</span>
+
+        </div>
+    </div>
+</template>
---
+++ vue front/src/components/icons/logoTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <span>
+        <a href="#" class="text-xl font-bold flex items-center lg:ml-2.5 bg-white px-5 rounded-md">
+            <img src="@/assets/logoImg.png" class="h-6 mr-2" alt="Windster Logo">
+            <span class="self-center whitespace-nowrap">B-BOARD</span>
+        </a>
+    </span>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
