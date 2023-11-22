89c685c6e336ca203c9c699ab7c858c485138e99
damdam6 <dambi626626@gmail.com>
2023-11-22 11:38:08 +0900

    feat: Pinia-상태관리(Main Api호출)
    Pinia - promise 작업
    Main api호출 후 db 분배

--- vue front/src/main.js
+++ vue front/src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueCookies from "vue-cookies"
+
 
 
 import App from './App.vue'
@@ -12,8 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueCookies);
-//쿠키의 만료일은 7일이다. (글로벌 세팅)
-app.$cookies.config("7d");
+
+
 
 app.mount('#app')
--- vue front/src/stores/db.js
+++ vue front/src/stores/db.js
@@ -1,19 +1,31 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-
+import axios from 'axios';
 
 export const dbStore = defineStore('allData', () => {
+  const fullObject = ref({'key':'value'}) 
 
-  const fullObject = ref({'key':'key'})  
+  const fetchData = async () => {
+    try {
+       const id = 1; // 아이디 설정할 것
+       axios({
+          url: `http://localhost:8080/api/main/group/${id}`,
+          method: "GET",
+          withCredentials: true,
+       }).then((response) => {
+          fullObject.value = response.data;
+       });
+    } catch (err) {
+      console.log(err.message);
+    }
+ };
 
   const userList = computed(() => {
     // fullObject.value.users가 존재하는지 확인
-    console.log(fullObject)
     if (fullObject.value && fullObject.value.users) {
       return fullObject.value.users;
     }
-    console.log('없다고 뜸')
-    return ['뭔데']; // fullObject.value.users가 없으면 빈 배열 반환
+    return []; // fullObject.value.users가 없으면 빈 배열 반환
   });
   
   const userMap = computed(() => {
@@ -23,17 +35,19 @@ export const dbStore = defineStore('allData', () => {
     }
     return new Map(); // userList.value가 없으면 빈 Map 반환
   });
-  const top100problemList = computed(() => fullObject.value.top100problems)
+  const top100problemList = computed(() =>{
+    console.log('tp100')
+   return fullObject.value.top100problems})
   const userTierproblemList = computed(() => fullObject.value.userTierProblems)
   const recomProblemList = computed(() => fullObject.value.recomProblems)
   const algorithmList = computed(() => fullObject.value.algorithms)
   const algorithmMap = computed(() => {
     if(!algorithmList.value)return {};
-    return new Map(algorithmList.value.map(item => [item.problem_num, item.algorithm]));
+    return new Map(algorithmList.value.map(item => [item.problemNum, item.algorithm]));
   });
   const userTop100problemList = computed( () => fullObject.value.userTop100problems
   )
   
-  return {fullObject, userList,
+  return {fetchData,fullObject, 
   userMap, top100problemList, userTierproblemList, recomProblemList, algorithmMap, userTop100problemList}
 })
