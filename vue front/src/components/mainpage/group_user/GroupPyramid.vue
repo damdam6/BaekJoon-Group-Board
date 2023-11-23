c2e315b7663dfcd9b00b7b3684ff90401654be94
damdam6 <dambi626626@gmail.com>
2023-11-23 16:34:43 +0900

    fix: 그룹 피라미드 수정
    알고리즘 수정
    노출 문제 순서 수정
    일부 문제 노출 오류 수정
    문제 - 링크 / hover 통일
    
    close: #27

--- vue front/src/components/mainpage/group_user/GroupPyramid.vue
+++ vue front/src/components/mainpage/group_user/GroupPyramid.vue
@@ -3,7 +3,8 @@
         <div v-for="level in 14" :key="level" class="flex justify-center mb-2 space-x-2">
             <div v-for="box in level" :key="level + '-' + box">
                 <a :href="getBoxLink(level, box)" target="_blank">
-                    <div :class="[getBoxClass(level, box), getBoxSize(level)]">
+                    <div :class="['border-2', getBoxClass(level, box), getBoxSize(level)]"
+                        @mouseover="handleMouseOver(level, box)" @mouseleave="handleMouseLeave">
                     </div>
                 </a>
             </div>
@@ -11,42 +12,61 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { mainApiStore } from '@/stores/main-api';
 import { selectedUserStore } from '@/stores/userInfo';
+import { changeboxStore } from '@/stores/changebox-db';
+const mainApiStoreInst = mainApiStore();
+const userInfoInst = selectedUserStore();
+const changeboxStoreInst = changeboxStore();
 
-export default {
-    setup() {
-        const mainApiStoreInst = mainApiStore();
-        const userInfoInst = selectedUserStore();
-
-        function getBoxClass(level, box) {
-            const idx = level * (level - 1) / 2 + box - 1;
-            const problemNum = mainApiStoreInst.top100problemList[idx].problemId;
-            const getUserId = userInfoInst.userId;
-            // userInfo의 problemNum 목록에 현재 problemNum이 있는지 확인합니다.
-            const isSolved = getUserId && mainApiStoreInst.setUserTop100[getUserId].includes(problemNum);
-            return isSolved ? 'bg-red-500' : 'bg-blue-500';
-        }
-
-        function getBoxSize(level) {
-            // 레벨에 따라 박스 크기를 조정합니다.
-            if (level < 5) {
-                return 'w-5 h-5 rounded-sm sm:rounded-md md:w-6 md:h-6'; // 20px
-            } else if (level === 5) {
-                return 'hidden w-5 h-5 rounded-sm sm:rounded-md md:w-6 md:h-6'
-            } else {
-                return 'w-4 h-4 rounded-sm sm:rounded-md sm:w-4 sm:h-4 md:w-5 md:h-5'; // 16px
-            }
-        }
-
-        function getBoxLink(level, box) {
-            const idx = level * (level - 1) / 2 + box - 1;
-            const problemNum = mainApiStoreInst.top100problemList[idx].problemId;
-            return `https://www.acmicpc.net/problem/${problemNum}`
-        }
-
-        return { mainApiStoreInst, userInfoInst, getBoxClass, getBoxSize, getBoxLink };
+const handleMouseOver = (level, box) => {
+    let idx = level * (level - 1) / 2 + box - 1;
+    if (level > 5) {
+        idx -= 5;
     }
+    changeboxStoreInst.overProblemNum = mainApiStoreInst.getTop100ProNum[idx];
+
+};
+
+const handleMouseLeave = () => {
+
+    if (changeboxStoreInst.overProblemNum.value) {
+        changeboxStoreInst.overProblemNum.value = 0;
+    }
+};
+
+
+const boxId = (level, box) => `${level}-${box}`;
+
+const getBoxClass = (level, box) => {
+    let idx = level * (level - 1) / 2 + box - 1;
+    if (level > 5) {
+        idx -= 5;
+    }
+    const problemNum = mainApiStoreInst.getTop100ProNum[idx];
+    const getUserId = userInfoInst.userId;
+    const isSolved = getUserId && mainApiStoreInst.setUserTop100[getUserId].includes(problemNum);
+    return isSolved ? 'hover:bg-red-500 border-red-500' : 'hover:bg-blue-500 border-blue-500';
+};
+
+
+const getBoxSize = (level) => {
+    if (level < 5) {
+        return 'w-5 h-5 rounded-sm sm:rounded-md md:w-6 md:h-6'; // 20px
+    } else if (level === 5) {
+        return 'hidden w-5 h-5 rounded-sm sm:rounded-md md:w-6 md:h-6';
+    } else {
+        return 'w-4 h-4 rounded-sm sm:rounded-md sm:w-4 sm:h-4 md:w-5 md:h-5'; // 16px
+    }
+};
+
+const getBoxLink = (level, box) => {
+    let idx = level * (level - 1) / 2 + box - 1;
+    if (level > 5) {
+        idx -= 5;
+    }
+    const problemNum = mainApiStoreInst.getTop100ProNum[idx];
+    return `https://www.acmicpc.net/problem/${problemNum}`;
 };
 </script>
