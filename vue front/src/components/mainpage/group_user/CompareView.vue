79b8bf122ffb1d83d3649305ffdd01d67ac00e65
damdam6 <dambi626626@gmail.com>
2023-11-23 16:30:58 +0900

    feat: 비교 view 정보 세팅
    close: #31

--- vue front/src/components/mainpage/group_user/CompareView.vue
+++ vue front/src/components/mainpage/group_user/CompareView.vue
@@ -1,5 +1,10 @@
 <template>
-    <UserProfile />
+    <div
+        :class="['p-5 text-gray-200 bg-gray-800 rounded-lg shadow sm:p-6 xl:p-8', userInfoInst.userId === 0 ? 'invisible' : '']">
+        <div>
+            <UserProfile class="bg-black" />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -16,23 +21,16 @@ export default {
     setup() {
         const mainApiStoreInst = mainApiStore();
         const userInfoInst = selectedUserStore();
-        const tableData = ref([
-            { tier: userInfoInst.userTier, top100Solved: 50, allSolved: 100, rank: 10 },
-            { tier: 2, top100Solved: 40, allSolved: 90, rank: 20 }
-        ]);
 
-        const metrics = ref([
-            { name: 'Tier', key: 'tier' },
-            { name: 'Top 100 Solved', key: 'top100Solved' },
-            { name: 'All Solved', key: 'allSolved' },
-            { name: 'Rank', key: 'rank' }
-        ]);
+
+        const setHidden = function () {
+            return "{'invisible': userInfoInst.userId === 0}"
+        }
 
         return {
             userInfoInst,
-            tableData,
-            metrics,
             mainApiStoreInst,
+            setHidden
 
         };
     }
