adea155df8ea97bdffb33d32996377fa073db144
damongsanga <mike0413lata@gmail.com>
2023-11-23 23:42:26 +0900

    feat: group select page 수정

--- vue front/src/views/GroupSelectView.vue
+++ vue front/src/views/GroupSelectView.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="store.isLoading">로딩중</div>
   <div v-else>
-    <div class="from-blue-900 via-indigo-800 to-indigo-500 bg-gradient-to-br">
+    <div class="h-screen from-blue-900 via-indigo-800 to-indigo-500 bg-gradient-to-br">
       <div class="pt-5 pl-5 flex">
         <p class="flex p-5 bg-slate-200 w-fit rounded items-center">
           <img
@@ -11,7 +11,7 @@
         <p class="pl-2 font-bold text-center">{{ store.loginUser.handle }}</p>
         </p>
       </div>
-      <div class="min-h-screen py-12 flex items-center justify-center">
+      <div class="py-12 flex items-center justify-center">
         <div
           v-if="!store.groupList.length"
           class="flex items-center justify-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
